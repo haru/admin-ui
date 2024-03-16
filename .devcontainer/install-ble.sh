503ccdec639f120a1838ef6269565aba87779c87
Haruyuki Iida <haru.ii.a9i1e@gmail.com>
2024-03-16 05:03:44 +0000

    devcontainer

---
+++ .devcontainer/install-ble.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+set -x
+cd /tmp
+git clone --recursive --depth 1 --shallow-submodules https://github.com/akinomyoga/ble.sh.git
+make -C ble.sh install PREFIX=~/.local
+echo 'source ~/.local/share/blesh/ble.sh' >> ~/.bashrc
---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+set -x
+
+cd `dirname $0`
+
+cp env.local ../.env.local
